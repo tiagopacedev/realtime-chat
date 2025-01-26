eda4b5fb8851ff8d08cbda4c86bfd172bf4f1e39
Tiago Pace <tiagopace.dev@gmail.com>
2025-01-26 15:42:41 -0300

    feat(layout): add sign-out button

---
+++ src/components/sign-out-button.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Loader2, LogOut } from 'lucide-react'
+import { signOut } from 'next-auth/react'
+import { toast } from 'sonner'
+import { useState } from 'react'
+
+import { Button } from './ui/button'
+
+export default function SignOutButton({ ...props }) {
+  const [isSigningOut, setIsSigningOut] = useState<boolean>(false)
+  return (
+    <Button
+      {...props}
+      variant="ghost"
+      className="mr-6 h-full w-12"
+      onClick={async () => {
+        setIsSigningOut(true)
+        try {
+          await signOut()
+        } catch (error) {
+          toast.error('There was a problem signing out')
+        } finally {
+          setIsSigningOut(false)
+        }
+      }}
+    >
+      {isSigningOut ? <Loader2 className="animate-spin" /> : <LogOut />}
+    </Button>
+  )
+}
