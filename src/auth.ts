d1d4b9a282937403b6991fc3e3cf737772dddc01
Tiago Pace <tiagopace.dev@gmail.com>
2025-03-19 15:11:03 -0300

    fix(auth): add string type assertion to session handling

--- src/auth.ts
+++ src/auth.ts
@@ -36,10 +36,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     async session({ session, token }) {
       if (token) {
-        session.user.id = token.id
-        session.user.name = token.name
-        session.user.email = token.email
-        session.user.image = token.picture
+        session.user.id = token.id as string
+        session.user.name = token.name as string
+        session.user.email = token.email as string
+        session.user.image = token.picture as string
       }
 
       return session
